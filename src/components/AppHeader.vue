bd16275d66acba49b44ad3e0cac66b99437f2f03
Bokskie <103017806+Bokskie@users.noreply.github.com>
2022-10-08 09:58:24 +0800

    Add files via upload

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,89 @@
+<template>
+    <header>
+        <div class="nav-bar">
+            <div class="sidebar-button">
+                <button @click="show"><span class="material-symbols-sharp">menu</span></button>
+            </div>
+            <div class="dashboard">
+                <h1>DashBoard</h1>
+            </div>
+            <div class="search-box">
+                <form action="" method="">
+                    <input type="search" placeholder="Search...">
+                    <button>
+                        <span class="material-symbols-sharp">search</span>
+                    </button>
+                </form>    
+            </div>
+            <div class="user-profile">
+                <span class="material-symbols-sharp">person</span>
+            </div>
+        </div>
+    </header>
+   
+</template>
+
+<script>
+   export default {
+       methods: {
+           show(){
+               this.$emit("buttonCliked")
+           }
+       }
+   }
+
+</script>
+
+<style scoped>
+
+header 
+{
+    background: #464646
+}
+header button
+{
+    cursor: pointer;
+}
+header .nav-bar 
+{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+header .sidebar-button button 
+{
+    background: #00aa6c;
+    border-radius: 3rem;
+    position: relative;
+    left: 1rem;
+    top: 1.5rem;
+    width: 4rem;
+    height: 3rem;
+}
+header .nav-bar .search-box
+{
+    background: #ffffff;
+    border-radius: 3rem;
+    position: relative;
+    top: 0.5rem;
+    padding: 10px;
+    height: 3rem;
+}
+header .nav-bar .search-box input 
+{
+    border: none;
+    outline: none;
+    border-radius: 3rem;
+    padding: 10px;
+    position: relative;
+    bottom: 0.2rem;
+}
+header .nav-bar .search-box button 
+{
+    border: none;
+    border-radius: 3rem;
+    padding: 10px;
+    background: #00aa6c;
+}
+</style>
---
+++ src/components/AppMainConent.vue
@@ -0,0 +1,209 @@
+<template>
+    <main>
+ 
+        <div class="main-content">
+
+                <div class="main-menu-branch">
+                    <h1>KAN-ANA</h1>
+                    <p>Menu</p>
+                    </div>
+
+            <section class="items">
+
+                <div class="item">
+                    <a href="">
+                        <img src="https://images.saymedia-content.com/.image/t_share/MTc0MTY5ODcyMTQ4NzM1NDg0/quick-recipe-for-philippine-bistek-the-filipino-beefsteak.jpg" jsaction="load:XAeZkd;" jsname="HiaYvf" class="n3VNCb KAlRDb" alt="How to Make Bistek Tagalog (Filipino Beefsteak) - Delishably" data-noaft="1" style="width: 120px;">
+                        <h4><span class="material-symbols-sharp">
+                            favorite
+                            </span>Beef Steak</h4>
+                        <p>50 Pesos Per Serve</p>
+                    </a>
+                </div>
+
+                <div class="item">
+                    <a href="">
+                        <img src="https://img-global.cpcdn.com/recipes/1c1d121ce9a48468/751x532cq70/pork-ginataan-recipe-main-photo.jpg" jsaction="load:XAeZkd;" jsname="HiaYvf" class="n3VNCb KAlRDb" alt="Recipe of Favorite Pork Ginataan | Super Easy Recipes" data-noaft="1" style="width: 120px;">
+                        <h4><span class="material-symbols-sharp">
+                            favorite
+                            </span>Ginataang Baboy</h4>
+                        <p>70 Pesos only Per Serve</p>
+                    </a>
+                </div>
+
+                <div class="item">
+                    <a href="">
+                        <img src="https://www.jayssweetnsourlife.com/wp-content/uploads/2017/01/filipino-pork-adobo-155-scaled-735x487.jpg" jsaction="load:XAeZkd;" jsname="HiaYvf" class="n3VNCb KAlRDb" alt="Pressure Cooker Pork Adobo - Jays Sweet N Sour Life" data-noaft="1" style="width: 120px;">
+                        <h4><span class="material-symbols-sharp">
+                            favorite
+                            </span>Adobong Baboy</h4>
+                        <p>60 Pesos only Per Serve</p>
+                    </a>
+                </div>
+
+                <div class="item">
+                    <a href="">
+                        <img src="https://momfoodblog.com/wp-content/uploads/2020/04/Matangbaka-bigeye-scad-Soup-MomFoodBlog-A05.jpg" jsaction="load:XAeZkd;" jsname="HiaYvf" class="n3VNCb KAlRDb" alt="Tinolang Isda with Malunggay (Fish Ginger Soup with Moringa Leaves)" data-noaft="1" style="width: 120px;">
+                        <h4><span class="material-symbols-sharp">
+                            favorite
+                            </span>Tinolang Isda</h4>
+                        <p>40 Pesos only for 1 Serve</p>
+                    </a>
+                </div>
+
+                <div class="item">
+                    <a href="">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO21FfnksmPHNrO1C_imhHEz0ThXnHkB_vqg&amp;usqp=CAU" jsaction="load:XAeZkd;" jsname="HiaYvf" class="n3VNCb KAlRDb" alt="One Pan Healthy &amp; Simple Breakfast Recipe" style="width: 120px;">
+                        <h4><span class="material-symbols-sharp">
+                            favorite
+                            </span>BreakFast Meal</h4>
+                        <p>50 Pesos only for whole meal</p>
+                    </a>
+                </div>
+
+                <div class="item">
+                    <a href="">
+                        <img src="https://omnivorescookbook.com/wp-content/uploads/2020/12/201113_Lumpia-Shanghai_550.jpg" jsaction="load:XAeZkd;" jsname="HiaYvf" class="n3VNCb KAlRDb" alt="Lumpia Shanghai (Filipino Spring Rolls) - Omnivore's Cookbook" data-noaft="1" style="width: 120px;">
+                        <h4><span class="material-symbols-sharp">
+                            favorite
+                            </span>Lumpia</h4>
+                        <p>15 Pesos only for 1 Pieces</p>
+                    </a>
+                </div>
+
+                <div class="item">
+                    <a href="">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnZ_f36yvXR8xA8Ph21w2SL3RTfHLUPuKzow&amp;usqp=CAU" jsaction="load:XAeZkd;" jsname="HiaYvf" class="n3VNCb KAlRDb" alt="Tortang Talong - Lutong Bahay Recipe" style="width: 120px;">
+                        <h4><span class="material-symbols-sharp">
+                            favorite
+                            </span>Tortang talong</h4>
+                        <p>20 Pesos only for 1 Pieces</p>
+                    </a>
+                </div>
+
+                <div class="item">
+                    <a href="">
+                        <img src="https://assets.epicurious.com/photos/57d70c8ade27564257b657c6/master/pass/perfect-steamed-rice.jpg" jsaction="load:XAeZkd;" jsname="HiaYvf" class="n3VNCb KAlRDb" alt="Perfect Steamed Rice Recipe | Epicurious" data-noaft="1" width="120" height="100">
+                        <h4><span class="material-symbols-sharp">
+                            favorite
+                            </span>Rice</h4>
+                        <p>10 Pesos only for 1 Serve</p>
+                    </a>
+                </div>
+
+                <div class="item">
+                    <a href="">
+                        <img src="https://www.lutongbahayrecipe.com/wp-content/uploads/2019/04/lutong-bahay-adobong-atay-ng-manok-1200x1310.jpg" jsaction="load:XAeZkd;" jsname="HiaYvf" class="n3VNCb KAlRDb" alt="Adobong Atay ng Manok - Lutong Bahay Recipe" data-noaft="1" width="120">
+                        <h4><span class="material-symbols-sharp">
+                            favorite
+                            </span>Adobong Atay ng Manok</h4>
+                        <p>30 Pesos only for 1 Serve</p>
+                    </a>
+                </div>
+
+
+            </section>
+     
+        </div>
+    </main>
+</template>
+
+<script>
+  
+
+</script>
+
+
+<style scoped>
+
+main 
+{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    background: #f7f7f7;
+    overflow-x: hidden;
+    justify-content: center;
+    text-decoration: none;
+    padding-left: 3rem;
+    list-style: none;
+}
+main .main-menu-branch
+{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+}
+main .main-menu-branch h1 
+{
+    font-family: sans-serif;
+}
+main .main-menu-branch p  
+{
+    font-family: sans-serif;   
+}
+main .items
+{
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+    flex-wrap: wrap;
+    position: relative;
+    top: 4rem;
+    gap: 3rem;
+    padding-bottom: 2rem;
+}
+main .items .item
+{
+    background: #20ffad;
+    border: 1px solid #000000;
+    border-radius: 3rem;
+    box-shadow: 1px 2px 3px 4px;
+    width: 190px;
+    height: 40vh;
+    padding: 10px;
+}
+main .items .item a 
+{
+    text-decoration: none;
+    color: #000000;
+}
+main .items .item a h4 
+{
+    font-family: sans-serif;
+    font-size: 22px;
+    color: #000000;
+}
+main .items .item a h4 span 
+{
+    background: rgb(255, 255, 255);
+    border-radius: 3rem;
+    padding: 0.5rem;
+    position: relative;
+    top: 0.2rem;
+}
+main .items .item a p 
+{
+    font-size: 18px;
+    font-family: Arial, Helvetica, sans-serif;
+}
+main .items .item a img 
+{
+    border-radius: 3rem;
+    color: #00aa6c;
+    box-shadow: 2px 4px 5px;
+    width: 120px;
+    height: 90px;
+}
+/*=========HOVER==========*/
+main .items .item:hover
+{
+    position: relative;
+    top: 10px;
+}
+main .items .item a h4 span:hover 
+{
+    background: rgb(255, 0, 0);
+    transition: all 0.5s ease;
+}
+</style>
